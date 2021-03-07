ddba864b02011e5e389778890e042a7ce317b43e
Alex S <alex.s@smarthuts.com>
2021-03-07 19:00:27 +0200

    Add fetch_movies.dart

---
+++ lib/feature/movie/domain/usecase/fetch_movies.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_clean_arhitecture/core/error/failures.dart';
+import 'package:flutter_clean_arhitecture/core/usecases/usecase.dart';
+import 'package:flutter_clean_arhitecture/feature/movie/data/repository/movie_repository.dart';
+import 'package:flutter_clean_arhitecture/feature/movie/domain/entities/movie.dart';
+
+class FetchMoviesUseCase extends UseCase<List<Movie>, NoParams> {
+
+  final MovieRepository repository;
+
+  FetchMoviesUseCase(this.repository);
+
+  @override
+  Future<Either<Failure, List<Movie>>> call(NoParams params) {
+    throw UnimplementedError();
+  }
+
+}
--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,6 +1,7 @@
 import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:flutter_clean_arhitecture/feature/movie/data/datasource/movie_local_source.dart';
 import 'package:flutter_clean_arhitecture/feature/movie/data/datasource/movie_remote_source.dart';
+import 'package:flutter_clean_arhitecture/feature/movie/domain/usecase/fetch_movies.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,10 +10,10 @@ import 'core/network/network_info.dart';
 final sl = GetIt.instance;
 
 Future<void> init() async {
-
   sl.registerFactory(() => MovieRemoteSource(sl()));
   sl.registerFactory(() => MovieLocalSource());
   sl.registerLazySingleton(() => http.Client());
   sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
   sl.registerLazySingleton(() => DataConnectionChecker());
+  sl.registerFactory(() => FetchMoviesUseCase(sl()));
 }
