ba016e24d7046e4da4f7133355d376fe05dccda6
Alex S <alex.s@smarthuts.com>
2021-03-07 18:55:43 +0200

    Add dependencies to injection_container.dart

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,4 +1,5 @@
 import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:flutter_clean_arhitecture/feature/movie/data/datasource/movie_local_source.dart';
 import 'package:flutter_clean_arhitecture/feature/movie/data/datasource/movie_remote_source.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
@@ -10,6 +11,7 @@ final sl = GetIt.instance;
 Future<void> init() async {
 
   sl.registerFactory(() => MovieRemoteSource(sl()));
+  sl.registerFactory(() => MovieLocalSource());
   sl.registerLazySingleton(() => http.Client());
   sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
   sl.registerLazySingleton(() => DataConnectionChecker());
